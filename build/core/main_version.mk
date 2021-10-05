beef52d2bf01ba16b31dc2cb1d2114c47592fcf1
Michael Bestas <mkbestas@lineageos.org>
2021-10-05 23:37:55 +0300

    build: Replace obsolete ADDITIONAL_BUILD_PROPERTIES
    Fixes:
    error: ADDITIONAL_BUILD_PROPERTIES is obsolete.  Please use ADDITIONAL_SYSTEM_PROPERTIES.
    Change-Id: I7eb3c47d9fd705aa8f8d39e10563e47ffbf2d560

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,11 +1,11 @@
 # Build fingerprint
 ifneq ($(BUILD_FINGERPRINT),)
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
 # LineageOS System Version
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.spiceos.version=$(SPICEOS_VERSION) \
     ro.spiceos.releasetype=$(SPICEOS_BUILDTYPE) \
     ro.spiceos.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
@@ -13,5 +13,5 @@ ADDITIONAL_BUILD_PROPERTIES += \
     ro.spiceoslegal.url=https://lineageos.org/legal
 
 # SpiceOS Platform Display Version
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.spiceos.display.version=$(SPICEOS_DISPLAY_VERSION)
