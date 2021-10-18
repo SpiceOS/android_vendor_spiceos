fe37168216610d579970bf36eea9d93727e1dd70
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-18 12:56:05 +0530

    build: SpiceOS bringup
    Co-authored-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -6,20 +6,12 @@ endif
 
 # LineageOS System Version
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(LINEAGE_VERSION) \
-    ro.lineagelegal.url=https://lineageos.org/legal
+    ro.spiceos.version=$(SPICEOS_VERSION) \
+    ro.spiceos.releasetype=$(SPICEOS_BUILDTYPE) \
+    ro.spiceos.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.modversion=$(SPICEOS_VERSION) \
+    ro.spiceoslegal.url=https://lineageos.org/legal
 
-# LineageOS Platform Display Version
+# SpiceOS Platform Display Version
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION)
-
-# LineageOS Platform SDK Version
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.build.version.plat.sdk=$(LINEAGE_PLATFORM_SDK_VERSION)
-
-# LineageOS Platform Internal Version
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.build.version.plat.rev=$(LINEAGE_PLATFORM_REV)
+    ro.spiceos.display.version=$(SPICEOS_DISPLAY_VERSION)
